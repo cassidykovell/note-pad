bbe5350a7b6e08245befd55f37df00e953b7f6cd
Cassidy <cassidy0kovell@gmail.com>
2024-03-08 12:36:24 -0800

    added a gitignore

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 const PORT = 3001;
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, '../../index.html'));
+  res.sendFile(path.join(__dirname, "../../index.html"));
 });
 
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, '../../notes.html'));
+  res.sendFile(path.join(__dirname, "../../notes.html"));
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
